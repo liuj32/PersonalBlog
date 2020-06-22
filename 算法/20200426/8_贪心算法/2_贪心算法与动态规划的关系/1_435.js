b33eac7608daea3a6d223ce3fee6fb7c19fdb9c3
liujie <593177117@qq.com>
2020-06-22 09:17:44 +0800

    branch-a

--- 算法/20200426/8_贪心算法/2_贪心算法与动态规划的关系/1_435.js
+++ 算法/20200426/8_贪心算法/2_贪心算法与动态规划的关系/1_435.js
@@ -3,6 +3,47 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-var eraseOverlapIntervals = function(intervals) {
 
+ /** 动态规划 */
+var eraseOverlapIntervals = function(intervals) {
+  var len = intervals.length
+  if (len == 0) {
+    return 0
+  }
+  intervals.sort((a, b) => a[0] - b[0])
+  var memo = [1]
+   for(var i=1;i<len;i++) {
+     memo[i] = 1
+     for(var j=0;j<i;j++) {
+       if (intervals[i][0] >= intervals[j][1]) {
+         memo[i] = Math.max(1+memo[j], memo[i])
+       }
+     }
+   }
+
+   return len - Math.max(...memo)
+};
+
+ /** 贪心 */
+var eraseOverlapIntervals = function(intervals) {
+  var len = intervals.length
+  if (len == 0) {
+    return 0
+  }
+  intervals.sort((a, b) => {
+    if (a[1]  == b[1]) {
+      return a[0] - b[0]
+    } else {
+      return a[1] - b[1]
+    }
+  })
+  var res = 1, pre = 0
+  for(var i=1;i<len;i++) {
+      if (intervals[i][0] >= intervals[pre][1]) {
+          res++
+          pre = i
+      }
+  }
+
+   return len - res
 };
